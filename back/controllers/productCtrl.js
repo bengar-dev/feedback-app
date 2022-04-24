5b1b8c3693be27875580f5f709c1a8db81faef54
benysserp <benoitgar.pro@gmail.com>
2022-04-24 10:26:30 +0200

    update: Product controllers

--- back/controllers/productCtrl.js
+++ back/controllers/productCtrl.js
@@ -28,3 +28,33 @@ exports.getProducts = (req, res, next) => { // GET ALL PRODUCTS
         .then((products) => res.status(200).json(products))
         .catch(error => res.status(500).json({error : error}), db.Product.sync())
 }
+
+exports.getProduct = (req, res, next) => { // GET ONE PRODUCT
+    db.Product.findOne({
+        where: {productId: req.params.id},
+        include: [{
+            model: db.User,
+            attributes: {exclue: ['password']}
+        }]
+    })
+        .then((product) => {
+            if(!product) {
+                return res.status(401).json({error : 'Product not found'})
+            }
+            res.status(200).json(product)
+        })
+        .catch(error => res.status(500).json({error : error}), db.Product.sync())
+}
+
+exports.delProduct = (req, res, next) => {
+    db.Product.findOne({
+        where: {productId: req.params.id}
+    })
+        .then((product) => {
+            if(!product) return res.status(401).json({error : 'Product not found'})
+            if(product.autorId !== req.body.autorId) return res.status(401).json({error : 'You are not the autor'})
+            db.Product.destroy({where: {productId: req.params.id}})
+                .then(() => res.status(200).json({message: 'Product deleted'}))
+                .catch(error => res.status(401).json({error: error}))
+        })
+}
--- back/routes/productRoutes.js
+++ back/routes/productRoutes.js
@@ -6,7 +6,10 @@ const productCtrl = require('../controllers/productCtrl.js')
 const auth = require('../middleware/auth.js')
 
 router.get('/', productCtrl.getProducts)
+router.get('/:id', productCtrl.getProduct)
 
 router.post('/', productCtrl.addProduct)
+
+router.delete('/:id', productCtrl.delProduct)
 
 module.exports = router
