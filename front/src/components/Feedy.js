1dc36684bb3fb02511e8dfd7e4fcdfa1caf9062d
benysserp <benoitgar.pro@gmail.com>
2022-04-27 19:11:10 +0200

    update: hover like btn

--- front/src/components/Feedy.js
+++ front/src/components/Feedy.js
@@ -19,6 +19,7 @@ export default function Feedy(props) {
   }))
 
   const likes = JSON.parse(props.like)
+  const findUser = likes.findIndex(p => p === token.userId)
 
   const handleLikes = (id) => {
     async function awaitLikes() {
@@ -43,12 +44,21 @@ export default function Feedy(props) {
   return (
     <div className="transition-all duration-500 flex mb-4 bg-white h-32 w-full rounded shadow-md p-4 text-cyan-900"  data-aos="fade-up" data-aos-duration='2000'>
         <div className="w-1/6 border-r flex flex-col items-center justify-center">
+          {findUser === -1 ?
           <button 
           onClick={(e) => e.preventDefault(handleLikes(props.id))}
-          className="transition-all duration-200 shadow-none bg-slate-300 hover:bg-slate-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium">
+          className="transition-all duration-200 shadow-none bg-slate-300 hover:bg-emerald-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium">
             <i className="fas fa-angle-up mb-1" />
             <span className="font-bold">{likes.length}</span>
           </button>
+          :
+          <button 
+          onClick={(e) => e.preventDefault(handleLikes(props.id))}
+          className="transition-all duration-200 shadow-none bg-emerald-300 hover:bg-slate-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium">
+            <i className="fas fa-angle-up mb-1" />
+            <span className="font-bold">{likes.length}</span>
+          </button>
+          }
         </div>
         <div className="relative w-4/6 p-2 flex flex-col space-y-2">
           <Link to='/' className="transiton-all duration-200 text-sm font-bold text-sky-900 hover:text-sky-500">{props.name}</Link>
