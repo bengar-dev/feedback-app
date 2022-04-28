791dd8899e7f3499ba7b451b994bd4c259545d6a
benysserp <benoitgar.pro@gmail.com>
2022-04-28 12:38:27 +0200

    update: refacto

--- front/src/components/Feedy.js
+++ front/src/components/Feedy.js
@@ -1,74 +1,85 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import { getLikes } from '../services/product'
+import { getLikes } from "../services/product";
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
 
 export default function Feedy(props) {
+  const token = JSON.parse(localStorage.getItem("token"));
 
-  const token = JSON.parse(localStorage.getItem('token'))
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+  const { productArray } = useSelector((state) => ({
+    ...state.productReducer,
+  }));
 
-  const {productArray} = useSelector(state => ({
-    ...state.productReducer
-  }))
-
-  const likes = JSON.parse(props.like)
-  const findUser = likes.findIndex(p => p === token.userId)
+  const likes = JSON.parse(props.like);
+  const findUser = likes.findIndex((p) => p === token.userId);
 
   const handleLikes = (id) => {
     async function awaitLikes() {
-      const result = await getLikes(id)
-      if(typeof result === 'string') {
-        console.log(result)
+      const result = await getLikes(id);
+      if (typeof result === "string") {
+        console.log(result);
       } else {
         const obj = {
           id,
-          userId: token.userId
-        }
+          userId: token.userId,
+        };
         dispatch({
-          type: 'LIKE',
-          payload: obj
-        })
+          type: "LIKE",
+          payload: obj,
+        });
       }
     }
-    awaitLikes()
-
-  }
+    awaitLikes();
+  };
 
   return (
-    <div className="transition-all duration-500 flex mb-4 bg-white hover:bg-slate-50 h-32 w-full rounded shadow-lg p-4 text-cyan-900"  data-aos="fade-up" data-aos-duration='2000'>
-        <div className="w-1/6 border-r flex flex-col items-center justify-center">
-          {findUser === -1 ?
-          <button 
-          onClick={(e) => e.preventDefault(handleLikes(props.id))}
-          className="transition-all duration-200 shadow-none bg-slate-300 hover:bg-emerald-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium">
+    <div
+      className="transition-all duration-500 flex mb-4 bg-white hover:bg-slate-50 h-32 w-full rounded shadow-lg p-4 text-cyan-900"
+      data-aos="fade-up"
+      data-aos-duration="2000"
+    >
+      <div className="w-1/6 border-r flex flex-col items-center justify-center">
+        {findUser === -1 ? (
+          <button
+            onClick={(e) => e.preventDefault(handleLikes(props.id))}
+            className="transition-all duration-200 shadow-none bg-slate-300 hover:bg-emerald-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium"
+          >
             <i className="fas fa-angle-up mb-1" />
             <span className="font-bold">{likes.length}</span>
           </button>
-          :
-          <button 
-          onClick={(e) => e.preventDefault(handleLikes(props.id))}
-          className="transition-all duration-200 shadow-none bg-emerald-300 hover:bg-slate-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium">
+        ) : (
+          <button
+            onClick={(e) => e.preventDefault(handleLikes(props.id))}
+            className="transition-all duration-200 shadow-none bg-emerald-300 hover:bg-slate-400 hover:text-white text-sky-900 w-12 h-12 rounded flex flex-col justify-center items-center text-sm font-medium"
+          >
             <i className="fas fa-angle-up mb-1" />
             <span className="font-bold">{likes.length}</span>
           </button>
-          }
-        </div>
-        <div className="relative w-4/6 p-2 flex flex-col space-y-2">
-          <Link to='/' className="transiton-all duration-200 text-sm font-bold text-sky-900 hover:text-sky-500">{props.name}</Link>
-          <p className="text-xs text-slate-500">{props.desc}</p>
-          <button className="absolute right-2 bottom-2 text-xs font-medium bg-sky-200 w-max p-1 rounded-lg">{props.username}</button>
-        </div>
-        <button className="transition-all duration-500 w-1/6 flex space-x-2 items-center justify-center hover:scale-125">
-            <i className="fas fa-comment text-slate-400" />
-            <span className="text-xs text-sky-800 font-bold">2</span>
+        )}
+      </div>
+      <div className="relative w-4/6 p-2 flex flex-col space-y-2">
+        <Link
+          to="/"
+          className="transiton-all duration-200 text-sm font-bold text-sky-900 hover:text-sky-500"
+        >
+          {props.name}
+        </Link>
+        <p className="text-xs text-slate-500">{props.desc}</p>
+        <button className="absolute right-2 bottom-2 text-xs font-medium bg-sky-200 w-max p-1 rounded-lg">
+          {props.username}
         </button>
+      </div>
+      <button className="transition-all duration-500 w-1/6 flex space-x-2 items-center justify-center hover:scale-125">
+        <i className="fas fa-comment text-slate-400" />
+        <span className="text-xs text-sky-800 font-bold">2</span>
+      </button>
     </div>
-  )
+  );
 }
--- front/src/containers/Home.js
+++ front/src/containers/Home.js
@@ -1,43 +1,40 @@
-import React, {useEffect} from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import Profil from '../components/Profil'
-import TopBoard from '../components/TopBoard'
-import Feedy from '../components/Feedy'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Profil from "../components/Profil";
+import TopBoard from "../components/TopBoard";
+import Feedy from "../components/Feedy";
 
-import { getProducts } from '../services/product'
-import BlockOptions from '../components/BlockOptions'
+import { getProducts } from "../services/product";
+import BlockOptions from "../components/BlockOptions";
 
 export default function Home() {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const {productArray} = useSelector(state => ({
-    ...state.productReducer
-  }))
+  const { productArray } = useSelector((state) => ({
+    ...state.productReducer,
+  }));
 
   useEffect(() => {
-    
     setTimeout(() => {
-      document.querySelector('main').classList.remove('opacity-0', 'scale-50')
-    }, 200)
+      document.querySelector("main").classList.remove("opacity-0", "scale-50");
+    }, 200);
 
     async function awaitGetProducts() {
-      const result = await getProducts()
-      if(typeof result === 'string') {
-        console.log(result)
+      const result = await getProducts();
+      if (typeof result === "string") {
+        console.log(result);
       } else {
         dispatch({
-          type: 'GETPRODUCTS',
-          payload: result
-        })
+          type: "GETPRODUCTS",
+          payload: result,
+        });
       }
     }
 
-    if(productArray.length <= 0) {
-      awaitGetProducts()
+    if (productArray.length <= 0) {
+      awaitGetProducts();
     }
-    
-  }, [])
+  }, []);
 
   return (
     <div className="min-h-screen bg-slate-100 pt-20">
@@ -47,26 +44,27 @@ export default function Home() {
           <BlockOptions />
         </div>
         <div className="w-2/3">
-         <TopBoard />
-         <div className="mt-6 w-full">
-          {productArray.length > 0 ?
-            productArray.map(item => 
-              <Feedy 
-              key={item.productId}
-              id={item.productId}
-              like={item.like}
-              username={item.User.username}
-              name={item.name}
-              desc={item.desc}
-              />
-              ) : 
+          <TopBoard />
+          <div className="mt-6 w-full">
+            {productArray.length > 0 ? (
+              productArray.map((item) => (
+                <Feedy
+                  key={item.productId}
+                  id={item.productId}
+                  like={item.like}
+                  username={item.User.username}
+                  name={item.name}
+                  desc={item.desc}
+                />
+              ))
+            ) : (
               <div className="transition-all duration-500 flex justify-center mb-4 bg-white h-max w-full rounded p-4 text-cyan-900">
-              <p className="font-medium text-sm">There is no products...</p>
+                <p className="font-medium text-sm">There is no products...</p>
               </div>
-          }
-         </div>
+            )}
+          </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
